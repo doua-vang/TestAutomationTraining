4d27b570bdcb72189094dbd5cd34b15567f3830d
dvang <vang.doua@outlook.com>
2020-02-20 15:12:16 -0600

    updating files

---
+++ java_course/src/chapter9/Employee.java
@@ -0,0 +1,24 @@
+package chapter9;
+
+public class Employee extends Person {
+    private String employeeId;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String title;
+
+}
