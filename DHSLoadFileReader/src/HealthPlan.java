71b4ab6d93d89d2cc7aa69747e400d55f5dc9740
Doua Vang <vang.doua@outlook.com>
2021-02-04 10:10:35 -0600

    adding webdriver

---
+++ DHSLoadFileReader/src/HealthPlan.java
@@ -0,0 +1,10 @@
+public class HealthPlan {
+
+    private String effectiveDate;
+    private String terminationDate;
+    private String medicarePartA;
+
+    public HealthPlan(String info) {
+
+    }
+}
--- DHSLoadFileReader/src/ReadFile.java
+++ DHSLoadFileReader/src/ReadFile.java
@@ -1,9 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 
 public class ReadFile {
+    static String newLine = "\n";
+    static String tabLine = "\t";
     public static void printArrayList(ArrayList<String> ids) {
         for(String id : ids) {
             System.out.println(id);
@@ -13,12 +16,20 @@ public class ReadFile {
         return word.replaceAll("\\s+$", "");
     }
 
-    public static ArrayList<String> getMemberIds(String line) {
-        ArrayList<String> ids = new ArrayList<String>();
-        ids.add(rightTrim(line.substring(7,14)));
-        ids.add(rightTrim(line.substring(15,34)));
-        ids.add(rightTrim(line.substring(35,45)));
-        ids.add(rightTrim(line.substring(46,57)));
+    public static String getAddressInformation(String line) {
+
+        return "";
+    }
+
+
+    public static String getMemberIds(String line) {
+        String ids = "";
+        ids += "HealthRulesId: " + rightTrim(line.substring(15,34)) + newLine;
+        ids += "AmisysId: " + rightTrim(line.substring(35,45)) + newLine;
+        ids += "PMI: " + rightTrim(line.substring(7,14)) + newLine;
+        ids += "CaseNumber: " + rightTrim(line.substring(237,245)) + newLine;
+        ids += "TuplNUmber: " + rightTrim(line.substring(245,264)) + newLine;
+        ids += "MBI: " + rightTrim(line.substring(46,57)) + newLine;
 
         return ids;
     }
@@ -35,6 +46,50 @@ public class ReadFile {
 
         return firstName + " " + middleName + " " + lastName;
     }
+
+    public static String getBirthDate(String line) {
+        return rightTrim(line.substring(178,185));
+    }
+
+    public static String getAddress(String line) {
+        String addressInformation = "";
+        String residentialAddressLineOne = rightTrim(line.substring(333,388));
+        String residentialAddressLineTwo = rightTrim(line.substring(388,443));
+        String residentialCityLine = rightTrim(line.substring(443,473));
+        String residentialStateLine = rightTrim(line.substring(473,476));
+        String residentialZipCodeLine = rightTrim(line.substring(476,490));
+        String correspondenceAddressLineOne = rightTrim(line.substring(701,756));
+        String correspondenceAddressLineTwo = rightTrim(line.substring(756,811));
+        String correspondenceCityLine = rightTrim(line.substring(811,841));
+        String correspondenceStateLine = rightTrim(line.substring(841,843));
+        String correspondenceZipCodeLine = rightTrim(line.substring(843,858));
+
+        String residentialAddress = "";
+        String correspondenceAddress = "";
+
+        if(residentialAddressLineTwo.equalsIgnoreCase("")) {
+            residentialAddress += tabLine + residentialAddressLineOne + newLine + tabLine + residentialCityLine + ", " + residentialStateLine + " " + residentialZipCodeLine;
+        }
+        else {
+            residentialAddress += tabLine + residentialAddressLineOne + newLine + tabLine + residentialAddressLineTwo + newLine + residentialCityLine + ", " + residentialStateLine + " " + residentialZipCodeLine;
+        }
+
+        if(correspondenceAddressLineOne.equals("")) {
+            correspondenceAddress = residentialAddress;
+        }
+        else {
+            if (correspondenceAddressLineTwo.equalsIgnoreCase("")) {
+                correspondenceAddress += tabLine + correspondenceAddressLineOne + newLine + tabLine + correspondenceCityLine + ", " + correspondenceStateLine + " " + correspondenceZipCodeLine;
+            } else {
+                correspondenceAddress += tabLine + correspondenceAddressLineOne + newLine + tabLine + correspondenceAddressLineTwo + newLine + correspondenceCityLine + ", " + correspondenceStateLine + " " + correspondenceZipCodeLine;
+            }
+        }
+
+        addressInformation = "Residential Address:" + newLine + residentialAddress + newLine + "Correspondence Address:" + newLine + correspondenceAddress;
+
+        return addressInformation;
+    }
+
     public static void main(String args[]) throws FileNotFoundException {
         String file = "UCARE_DHS_TRANSEND_20201102.txt";
         String filename = "C:\\Users\\dvang\\Desktop\\";
@@ -42,12 +97,21 @@ public class ReadFile {
 
         File dhsFile = new File(filename);
         Scanner s = new Scanner(dhsFile);
+        ArrayList<Person> members = new ArrayList<>();
 
         while(s.hasNextLine()) {
             String line = s.nextLine();
-            printArrayList(getMemberIds(line));
-            System.out.println(getName(line));
+
+            members.add(new Person(line));
+
         }
+
+        for(Person member : members) {
+            System.out.println(member.getFirstName());
+            System.out.println(member.getLastName());
+        }
+
+
     }
 
 }
